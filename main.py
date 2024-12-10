925f52b4338904d20a39b07f00fd7775b16cbb7d
ahmed77mostafa <ahmedmostafa.0522030@gmail.com>
2024-12-10 15:30:25 +0200

    Push Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+video = cv2.VideoCapture(r'236893_small.mp4')
+
+def video_brightness_contrast(frame,contrast,brightness):
+    return cv2.convertScaleAbs(frame, alpha = 1 + contrast / 127, beta=brightness)
+
+noise_kernel = (5,5)
+enhanced_kernel = np.array([
+    [-1, -1, -1],
+    [-1,  9, -1],
+    [-1, -1, -1]
+])
+while True:
+    res,frame = video.read()
+    if not res:
+        break
+    adjusted_frame = video_brightness_contrast(frame,contrast = 10, brightness = 5)
+    noise_frame = cv2.GaussianBlur(frame,noise_kernel,0)
+    enhanced_frame = cv2.filter2D(frame, -1, enhanced_kernel)
+
+    cv2.imshow('Adjusted Frame',adjusted_frame)
+    cv2.imshow('Noise Reduction',noise_frame)
+    cv2.imshow('Edge-Enhanced Frame', enhanced_frame)
+    cv2.waitKey(1)
